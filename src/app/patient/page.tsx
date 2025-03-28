94046ee7f66695daceb0aa7c486a70730f421dda
Silvia Abe <silviaabebuzatto@gmail.com>
2025-03-28 11:20:37 -0300

    feat: create patient page with image variation

---
+++ src/app/patient/page.tsx
@@ -0,0 +1,5 @@
+import AuthPage from "@/components/AuthPage/authPage";
+
+export default function Patient() {
+  return <AuthPage imageSrc="https://paciente.lacreisaude.com.br/_next/static/media/illustration-login.17db3f05.svg" />;
+}
